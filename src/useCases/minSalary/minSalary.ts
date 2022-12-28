0c6adb0a305da5a186a69c5974fbdaa3d7c40ed6
GuWuelta <rm93449@fiap.com.br>
2022-12-28 18:02:56 -0300

    fix: if additional removed

--- src/useCases/minSalary/minSalary.ts
+++ src/useCases/minSalary/minSalary.ts
@@ -3,15 +3,14 @@ import { GetRandomNumber } from "../../utils/randomNumber/getRandomNumber";
 class MinSalary {
   constructor() {}
 
-  getRandomSalary(role: string): number {
-    if (role != "worker" && role != "manager") throw new Error("Invalid role");
+  static getRandomSalary(role: string): number {
+    if (role != "worker" && role != "manager") throw new Error("Invalid role!");
     const randomNumber = new GetRandomNumber();
     const minSalary = {
       worker: randomNumber.execute(750, 2500),
       manager: randomNumber.execute(1400, 4600),
     }[role];
 
-    if (role != "worker" && role != "manager") throw new Error("Invalid role");
     //minSalary.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
     return minSalary;
   }
